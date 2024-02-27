09b6c5da7e93202c2f1f88e2f4c78b0f6e66a3f4
subhasis-chand <60152943+subhasis-chand@users.noreply.github.com>
2024-02-27 13:17:26 +0000

    adding react install component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
+
+  useEffect(() => {
+    const handler = e => {
+      e.preventDefault();
+      console.log("we are being triggered :D");
+      setSupportsPWA(true);
+      setPromptInstall(e);
+    };
+    window.addEventListener("beforeinstallprompt", handler);
+
+    return () => window.removeEventListener("transitionend", handler);
+  }, []);
+
+  const onClick = evt => {
+    evt.preventDefault();
+    if (!promptInstall) {
+      return;
+    }
+    promptInstall.prompt();
+  };
+  if (!supportsPWA) {
+    return null;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +37,16 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p id="demo">test after adding button script to script</p>
-        <button id="installApp">Install</button>
-        
+        <button
+          className="link-button"
+          id="setup_button"
+          aria-label="Install app"
+          title="Install app"
+          onClick={onClick}
+        >
+          Install component
+        </button>
+
       </header>
     </div>
   );
